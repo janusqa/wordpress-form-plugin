c811299af479db6171a91c41c0813e8de57bfbd6
Janus Developer <dev@retrievo.net>
2024-05-22 18:43:14 -0400

    add custom post type

--- src/wp-content/plugins/simple-contact-form/simple-contact-form.php
+++ src/wp-content/plugins/simple-contact-form/simple-contact-form.php
@@ -11,4 +11,32 @@
 
 if (!defined('ABSPATH')) exit;
 
-echo "Hello";
+class SimpleContactForm
+{
+
+    public function __construct()
+    {
+        add_action('init', array($this, 'create_custom_post_type'));
+    }
+
+    public function create_custom_post_type()
+    {
+        $args = array(
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array('title'),
+            'eclude_from_search' => true,
+            'publicly_querable' => false,
+            'capability' => 'manage_options',
+            'labels' => array(
+                'name' => 'Contact Form',
+                'singular_name' => 'Contact Form Entry'
+            ),
+            'menu_icon' => 'dashicons-media-text' // Dashicons | Wordpress Developer Resources
+        );
+
+        register_post_type('simple_contact_form', $args);
+    }
+}
+
+new SimpleContactForm;
