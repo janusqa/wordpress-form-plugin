38471d9a3dcc5f94f4233db35a9e0e2592504377
Janus Developer <dev@retrievo.net>
2024-05-22 17:05:57 -0400

    add simple theme and scaffold plugin

---
+++ src/wp-content/plugins/simple-contact-form/simple-contact-form.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: Simple Contact Form
+ * Description: Simple Contact Form Plugin
+ * Author: JanusQA
+ * Author URI: blog.retrievo.net
+ * Version: 1.0.0
+ * Text Domain: simple-contact-form
+ */
