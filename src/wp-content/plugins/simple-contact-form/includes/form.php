a80238c188423fa53df28036f3724533fb4ae244
Janus Developer <dev@retrievo.net>
2024-05-23 10:02:15 -0400

    make endpoint work

--- src/wp-content/plugins/simple-contact-form/includes/form.php
+++ src/wp-content/plugins/simple-contact-form/includes/form.php
@@ -1,8 +1,33 @@
 <?php
 
 add_shortcode('simple_contact_form', 'display_form');
+add_action('rest_api_init', 'process_form_rest_api_endpoint');
 
 function display_form()
 {
-    return 'load form';
+    include(SIMPLE_CONTACT_FORM_PLUGIN_PATH . '/includes/templates/form.php');
+}
+
+function process_form_rest_api_endpoint()
+{
+    register_rest_route('v1/simple-contact-form', 'process_form', array(
+        'methods' => 'POST',
+        'callback' => 'process_form'
+    ));
+}
+
+function process_form(WP_REST_Request $request)
+{
+    $data = array(
+        'name' => $request->get_param('name'),
+        'email' => $request->get_param('email'),
+        'phone' => $request->get_param('phone'),
+        'message' => $request->get_param('message'),
+    );
+
+    $response = new WP_REST_Response();
+    $response->set_data($data);
+    $response->set_headers(array('Content-type' => 'application/json'));
+    $response->set_status(200);
+    return $response;
 }
---
+++ src/wp-content/plugins/simple-contact-form/includes/templates/form.php
@@ -0,0 +1,27 @@
+<form id="simple_contact_form" style="display:flex; flex-direction:column; gap: 2px; width:25%">
+    <label>Name:</label>
+    <input type="text" name="name">
+    <label>Email:</label>
+    <input type="text" name="email">
+    <label>Phone:</label>
+    <input type="text" name="phone">
+    <label>Message:</label>
+    <textarea name="message"></textarea>
+    <button type="submit">Submit</button>
+</form>
+
+<script>
+    document.getElementById("simple_contact_form").addEventListener("submit", process_form);
+
+    function process_form(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        fetch("<?php echo get_rest_url(null, "v1/simple-contact-form/process_form"); ?>", {
+            method: "POST",
+            body: formData
+        }).then(response => {
+            if (!response.ok) throw new Error(`HTTP Error! Status: ${response.status}`);
+        }).catch(error => console.log(`Simple Contact Form Error: ${error.message}`));
+    }
+</script>
