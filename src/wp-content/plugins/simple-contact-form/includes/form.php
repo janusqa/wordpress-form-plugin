92b0fd3c9ec4cc546b55f67b45b4720fda407aff
Janus Developer <dev@retrievo.net>
2024-05-23 16:21:36 -0400

    build out custom post type to capture form submission

--- src/wp-content/plugins/simple-contact-form/includes/form.php
+++ src/wp-content/plugins/simple-contact-form/includes/form.php
@@ -1,7 +1,10 @@
 <?php
 
 add_shortcode('simple_contact_form', 'display_form');
-add_action('rest_api_init', 'process_form_rest_api_endpoint');
+add_action('rest_api_init', 'process_form_rest_api_endpoint'); // hook our function into wp as rest endpoint
+add_action('init', 'create_post_type'); // create custom post type for the plugin
+add_action('add_meta_boxes', 'create_meta_box');
+
 
 function display_form()
 {
@@ -39,8 +42,17 @@ function process_form(WP_REST_Request $request)
     $subject = "New enquiry from {$data['name']}";
 
     $message = "<h1>Message has been sent from {$data['name']}</h1>";
+
+    $postarr = [
+        'post_title' => $data['name'],
+        'post_type' => 'SCForm Submission',
+        'post_status' => 'publish' // enter post and set as published. It now will not show as draft in the post types
+    ];
+    $post_id = wp_insert_post($postarr);
+
     foreach ($data as $key => $value) {
         $message .= "<strong>" . ucfirst($key) . ":</strong>&nbsp;" . $value . "<br/>";
+        add_post_meta($post_id, $key, $value);
     }
 
     $response = new WP_REST_Response();
@@ -54,4 +66,45 @@ function process_form(WP_REST_Request $request)
         $response->set_status(500);
     }
     return $response;
+}
+
+function create_post_type()
+{
+    $args = [
+        'public' => true,
+        'has_archive' => true,
+        'labels' => [
+            'name' => 'SCForm Submissions',
+            'singular_name' => 'SCForm Submission'
+        ],
+        'capability_type' => 'post',
+        'capabilities' => [
+            'create_posts' => false // remove the add button on the custom post page
+        ],
+        'map_meta_cap' => true, // set to false, if users are not allowed to edit/delete existing posts
+        'supports' => false
+
+    ];
+
+    register_post_type('SCForm Submission', $args);
+}
+
+function create_meta_box()
+{
+    add_meta_box('simple_contact_form', 'SCForm Submission', 'display_submission', 'SCForm Submission');
+}
+
+function display_submission()
+{
+    $post_meta = get_post_meta(get_the_ID());
+    unset($post_meta['_edit_lock']);
+
+    echo "<ul>";
+    foreach ($post_meta as $key => $value) {
+        echo "<li><strong>" . ucfirst($key) . ":</strong><br/>" . $value[0] . "</li>";
+    }
+    echo "</ul>";
+
+    // example of hardcoding the meta (without looping)
+    // echo "Name:" . get_post_meta(get_the_ID(), 'name', true);
 }
--- src/wp-content/plugins/simple-contact-form/includes/templates/form.php
+++ src/wp-content/plugins/simple-contact-form/includes/templates/form.php
@@ -1,6 +1,6 @@
-<div>
-    <div id="form_submission_status" style=" display:none"></div>
-    <form id="simple_contact_form" style="display:flex; flex-direction:column; gap: 2px; width:25%">
+<div style="width:25%">
+    <div id="form_submission_status" style="display:none; padding:0.5em; margin-block-end:0.5em; border-radius:0.5em"></div>
+    <form id="simple_contact_form" style="display:flex; flex-direction:column; gap: 2px;">
         <?php wp_nonce_field('wp_rest'); ?> <!-- portect form from bots -->
         <label>Name:</label>
         <input type="text" name="name">
@@ -10,7 +10,7 @@
         <input type="text" name="phone">
         <label>Message:</label>
         <textarea name="message"></textarea>
-        <button type="submit">Submit</button>
+        <button type="submit" style="margin-block-start:0.5em">Submit</button>
     </form>
 </div>
 
@@ -38,9 +38,13 @@
         }).then(data => {
             simple_contact_form.style.display = "none";
             form_submission_status.textContent = data.message;
+            form_submission_status.style.background = "green";
+            form_submission_status.style.color = "white";
             form_submission_status.style.display = "block";
         }).catch(error => {
             form_submission_status.textContent = `Simple Contact Form Error: ${error.message}`;
+            form_submission_status.style.background = "red";
+            form_submission_status.style.color = "white";
             form_submission_status.style.display = "block";
         });
     }
